76601897ca0ee7bf86f92a4fb59c97e79c40f43b
Zjy <1163247099@qq.com>
2023-02-20 21:52:38 +0800

    first commit

---
+++ src/main/java/com/you/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.you.git;
+
+/**
+ * @Author chenghan
+ * @date 2023/2/20 - 21:47
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git!!");
+    }
+
+}
